1cb196a4bebae13d16d428e3c33691728b76374a
Sakura Phansiri <you@example.com>
2021-01-10 03:32:22 -0600

    Add new changes to file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
+import { Satellite } from './satellite';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'orbit-report';
+  sourceList: Satellite[];
+  displayList: Satellite[];
+  
+  constructor() {
+    this.sourceList = [];
+    this.displayList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+         response.json().then(function(data) {
+ 
+            let fetchedSatellites = data.satellites;
+          
+            for(let i = 0; i < fetchedSatellites.length; i++){
+               let satellite0 = {
+                  name: fetchedSatellites[i].name, 
+                  type: fetchedSatellites[i].type, 
+                  launchDate: fetchedSatellites[i].launchDate, 
+                  orbitType: fetchedSatellites[i].orbitType, 
+                  operational: fetchedSatellites[i].operational
+               }
+             this.sourceList.push(satellite0);
+            }  
+                 
+            this.displayList = this.sourceList.slice(0);
+ 
+         }.bind(this));
+
+      }.bind(this));
+   }
+
+   search(searchTerm: string): void {
+      let matchingSatellites: Satellite[] = [];
+      searchTerm = searchTerm.toLowerCase();
+      for(let i=0; i < this.sourceList.length; i++) {
+         let name = this.sourceList[i].name.toLowerCase();
+         if (name.indexOf(searchTerm) >= 0) {
+            matchingSatellites.push(this.sourceList[i]);
+         }
+      }
+      // assign this.displayList to be the array of matching satellites
+      // this will cause Angular to re-make the table, but now only containing matches
+      this.displayList = matchingSatellites;
+   }
+   
 }
+
---
+++ src/app/satellite.ts
@@ -0,0 +1,25 @@
+export class Satellite {
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+    
+    // This commented section has been replaced with a for loop in the orbit-list html:
+    // constructor (name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+    //     this.name = name;
+    //     this.type = type;
+    //     this.launchDate = launchDate;
+    //     this.orbitType = orbitType;
+    //     this.operational = operational;
+    // }
+   
+    shouldShowWarning(): boolean {
+        if (this.type.toLocaleLowerCase() === 'space debris'){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
